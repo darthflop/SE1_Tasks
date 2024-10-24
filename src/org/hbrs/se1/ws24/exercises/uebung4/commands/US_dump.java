501500da8b35e7b46ac1e7186b4eb53e036cd5ac
darthflop <filip3016@googlemail.com>
2024-10-24 19:58:11 +0200

    dump sorted list

--- src/org/hbrs/se1/ws24/exercises/uebung4/commands/US_dump.java
+++ src/org/hbrs/se1/ws24/exercises/uebung4/commands/US_dump.java
@@ -3,6 +3,10 @@ package org.hbrs.se1.ws24.exercises.uebung4.commands;
 import org.hbrs.se1.ws24.exercises.uebung4.Container;
 import org.hbrs.se1.ws24.exercises.uebung4.UserStories;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class US_dump implements US_Command {
 
     Container c;
@@ -15,15 +19,20 @@ public class US_dump implements US_Command {
     public void execute() {
 
         // Kopfzeile der Tabelle
-        System.out.printf("%-15s %-20s %-25s %-10s %-10s %-10s %-10s %-20s\n",
-                "ID", "Titel", "Akzeptanzkriterium", "Mehrwert", "Strafe", "Risiko", "Aufwand", "Projekt");
-        System.out.println("---------------------------------------------------------------------------------------------------------------------");
+        System.out.printf("%-15s %-20s %-25s %-10s %-10s %-10s %-10s %-20s %-10s\n",
+                "ID", "Titel", "Akzeptanzkriterium", "Mehrwert", "Strafe", "Risiko", "Aufwand", "Projekt", "Priorität");
+        System.out.println("------------------------------------------------------------------------------------------------------------------------------------------------");
+
+        List<UserStories> sorted = c.getCurrentList();
+
+        // Sortieren nach Priorität
+        Collections.sort(sorted, Comparator.comparingDouble(UserStories::getPrio).reversed());
 
         // Inhalte der User Stories in tabellarischer Form ausgeben
-        for (UserStories us : c.getCurrentList()) {
-            System.out.printf("%-15s %-20s %-25s %-10d %-10d %-10d %-10d %-20s\n",
+        for (UserStories us : sorted) {
+            System.out.printf("%-15s %-20s %-25s %-10d %-10d %-10d %-10d %-20s %-2.3f\n",
                     us.getID(), us.getTitel(), us.getAkzeptanzKriterien(), us.getMehrwert(), us.getStrafe(),
-                    us.getRisiko(), us.getAufwand(), us.getProjekt());
+                    us.getRisiko(), us.getAufwand(), us.getProjekt(), us.getPrio());
 
         }
     }
